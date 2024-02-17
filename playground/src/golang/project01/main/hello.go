3c2612d6cd16661a354d86545e249ba531d90500
MSI\Annie Kuo <a880318@gmail.com>
2024-02-17 22:57:00 +0800

    hello.go

---
+++ playground/src/golang/project01/main/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+func main(){
+
+	fmt.Println("Hello World!!")
+}
+
+// 要到 \golang-playground\playground\src\golang\project01\main 路徑底下執行
+
+// go build hello.go
+// 會編譯並產出 exe 執行檔
+
+// 直接執行程式(較慢)
+// go run hello.go
